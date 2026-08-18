a1381f0e1de3f67d70f7bf05ce9a964ce846de46
Aayush <aayush@Aayushs-MacBook-Air.local>
2026-08-18 12:31:00 +0530

    utils addition

---
+++ src/app.js
@@ -0,0 +1,25 @@
+import express from "express"
+import cookieParse from "cookie-parse"
+import cors from "cors"
+
+const app=express();
+
+app.use(cors({
+    origin:process.env.CORS_ORIGIN,
+    credentials:true
+}))
+
+app.use(express.json({
+    limit:"16kb"
+}))
+//multer , bodyParser
+
+app.use(express.urlencoded({
+    extended:true,limit:"16kb"
+}))
+
+app.use(express.static("public"))
+app.use(cookieParse())
+
+
+export {app}
--- src/index.js
+++ src/index.js
@@ -10,8 +10,15 @@ dotenv.config({
 })
  
 
-connectDB();
-
+connectDB()
+.then(()=>{
+    app.listen(process.env.PORT || 8000,()=>{
+        console.log(`o | server is running at port ${process.env.PORT} `)
+    })
+})
+.catch((err)=>{
+    console.log("conn failed mongodb error",error)
+})
 
 
 
