c831ee88e60021bf67f4e1b195f95dee11e06085
0xAayushh <1206aayush@gmail.com>
2026-08-19 12:50:31 +0530

    updated corrected code

--- src/models/video.model.js
+++ src/models/video.model.js
@@ -1,46 +1,81 @@
-import mongoose, { mongo, Schema } from "mongoose";
+import mongoose, { Schema } from "mongoose";
+
 import mongooseAggregatePaginate from "mongoose-aggregate-paginate-v2";
 
 const videoSchema = new Schema({
+
     videoFile:{
+
         type:String, //cloud url
+
         required:true,
+
     },
+
     thumbNail:{
+
         type :String, //cloud url
+
         required:true
+
     },
-     title:{
+
+    title:{
+
         type :String, //cloud url
+
         required:true
+
     },
-     description:{
+
+    description:{
+
         type :String, 
+
         required:true
+
     },
-     duration:{
+
+    duration:{
+
         type :Number, //cloud 
+
         required:true
+
     },
+
     views:{
+
         type:Number,
+
         default:0
+
     },
+
     isPublished:{
+
         type:Boolean,
+
         default:true
+
     },
+
     owner:{
+
         type:Schema.Types.ObjectId,
+
         ref:"User"
+
     }
 
 },
+
 {
+
     timestamps:true
+
 })
 
 videoSchema.plugin(mongooseAggregatePaginate);
 
-export const Video = mongoose.Schema("Video", videoSchema);
-    
+export const Video = mongoose.model("Video", videoSchema);
--- src/utils/ApiError.js
+++ src/utils/ApiError.js
@@ -1,24 +1,22 @@
-class apiError extends Error{
-    constructor
-    (
+class apiError extends Error {
+    constructor(
         statusCode,
-        message="something went wrong",
-        statck = ""
-    ){
-        super(message)
-        this.statusCode=statusCode
-        this.data=null
-        this.message
-        this.success=false;
-        this.errors=this.errors
+        message = "something went wrong",
+        stack = ""
+    ) {
+        super(message);
 
-        if(statck){
-            this.stack=statck
-        }
-        else{
-            Error.captureStackTrace(this,this.constructor)
+        this.statusCode = statusCode;
+        this.data = null;
+        this.success = false;
+        this.errors = [];
+
+        if (stack) {
+            this.stack = stack;
+        } else {
+            Error.captureStackTrace(this, this.constructor);
         }
     }
 }
 
-export {apiError}
+export { apiError };
