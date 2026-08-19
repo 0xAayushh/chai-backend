5453301156d26c1db62c71437c81071445d1fb5e
0xAayushh <1206aayush@gmail.com>
2026-08-19 20:59:56 +0530

    file handle

--- src/utils/cloudinary.js
+++ src/utils/cloudinary.js
@@ -6,3 +6,24 @@ cloudinary.config({
     api_key:env.process.CLOUDINARY_API_KEY,
     api_secret:CLOUDINARY_API_SECRET
 })
+
+const uploadOnCloudinary =async (localFilePath) =>{
+    try{
+        if(!localFilePath) return null
+
+         const response =await cloudinary.uploader.upload(localFilePath,{
+            resource_type:"auto"
+        })
+        console.log("file upload on cloudinary",response.url)
+        return response
+    }
+    catch(error){
+        fs.unlinkSync(localFilePath) //remove the local saved temp file
+        return null 
+
+    }
+}
+
+// cloudinary.v2.uploader.upload("/home/my_image.jpg", {upload_preset: "my_preset"}, (error, result)=>{
+//   console.log(result, error);
+// });
